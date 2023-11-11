3ead853d650a6c47764116b1d0b1ac1f48cc4a4e
rruiz88 <roman.ruiz816@gmail.com>
2023-11-11 11:45:03 -0600

    first commit

---
+++ App.tsx
@@ -0,0 +1,89 @@
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaView } from "react-native";
+import PersonalInfo from "./components/PersonalInfo";
+import Styles from "./components/Styles";
+import React, { useState, useCallback, useEffect } from "react";
+import Chat from "./components/Chat";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import * as SplashScreen from "expo-splash-screen";
+
+// Keep the splash screen visible while we fetch resources
+SplashScreen.preventAutoHideAsync();
+
+export default function App() {
+  const storageUserNameKey = "chatapp-username";
+  const storageImageKey = "chatapp-image";
+  const [username, setUsername] = useState("");
+  const [image, setImage] = useState("");
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        // Pre-load fonts, make any API calls you need to do here
+        const fetchPersonalData = async () => {
+          let fetchedUsername = await AsyncStorage.getItem(storageUserNameKey);
+          let username = fetchedUsername == null ? "" : fetchedUsername;
+          let fetchedImage = await AsyncStorage.getItem(storageImageKey);
+          let image = fetchedImage == null ? "" : fetchedImage;
+          setUsername(username);
+          setImage(image);
+        };
+        // Artificially delay for two seconds to simulate a slow loading
+        // experience. Please remove this if you copy and paste the code!
+        await new Promise((resolve) => setTimeout(resolve, 2000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        // Tell the application to render
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      // This tells the splash screen to hide immediately! If we call this after
+      // `setAppIsReady`, then we may see a blank screen while the app is
+      // loading its initial state and rendering its first pixels. So instead,
+      // we hide the splash screen once we know the root view has already
+      // performed layout.
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
+
+  const onPersonalInfoClosed = async (name: string, image: string) => {
+    setUsername(name);
+    await AsyncStorage.setItem(storageUserNameKey, name);
+    setImage(image);
+    await AsyncStorage.setItem(storageImageKey, image);
+  };
+
+  // if (isLoading) {
+  //   return (
+  //     <AppLoading
+  //       startAsync={fetchPersonalData}
+  //       onFinish={() => setIsLoading(false)}
+  //       onError={(error) => console.log(error)}
+  //     />
+  //   );
+  // }
+  let activeComponent =
+    username != "" ? (
+      <Chat username={username} image={image} />
+    ) : (
+      <PersonalInfo onClosed={onPersonalInfoClosed} />
+    );
+  return (
+    <SafeAreaView style={Styles.container} onLayout={onLayoutRootView}>
+      {activeComponent}
+      <StatusBar style="auto" />
+    </SafeAreaView>
+  );
+}
